04ab8e6ee76393b9ddfe4b874a701933a40642c9
Quang Vo <kanglaive@users.noreply.github.com>
2017-10-29 18:56:00 -0400

    Firebase Setup

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -10,9 +10,6 @@ public class HomeController extends Controller {
 
     /**
      * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
         return ok(views.html.index.render());
